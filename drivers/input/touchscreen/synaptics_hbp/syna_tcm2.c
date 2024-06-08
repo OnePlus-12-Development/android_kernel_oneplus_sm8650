eabb30f5741db57205350816f2b1ac5f010a323e
Dr-chen99 <dakaizhe@gmail.com>
2024-06-08 18:41:22 +0530

    synaptics_hbp: syna_tcm2: report double tap as KEY_WAKEUP
    Change-Id: I9aa5af35949f7d045fbf70b024315a48c6527c0f
    Signed-off-by: inferno0230 <mail@inferno0230.in>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- drivers/input/touchscreen/synaptics_hbp/syna_tcm2.c
+++ drivers/input/touchscreen/synaptics_hbp/syna_tcm2.c
@@ -820,9 +820,9 @@ static void syna_dev_report_input_events(struct syna_tcm *tcm)
 					touchpanel_event_call_notifier(EVENT_ACTION_UNDER_WATER, (void *)&tcm->under_water);
 				}
 			} else {
-				input_report_key(input_dev, KEY_F4, 1);
+				input_report_key(input_dev, KEY_WAKEUP, 1);
 				input_sync(input_dev);
-				input_report_key(input_dev, KEY_F4, 0);
+				input_report_key(input_dev, KEY_WAKEUP, 0);
 				input_sync(input_dev);
 			}
 		}
@@ -984,8 +984,8 @@ static int syna_dev_create_input_device(struct syna_tcm *tcm)
 
 	set_bit(KEY_SLEEP, input_dev->keybit);
 #ifdef ENABLE_WAKEUP_GESTURE
-	set_bit(KEY_F4, input_dev->keybit);
-	input_set_capability(input_dev, EV_KEY, KEY_F4);
+	set_bit(KEY_WAKEUP, input_dev->keybit);
+	input_set_capability(input_dev, EV_KEY, KEY_WAKEUP);
 	set_bit(KEY_UNDER_WATER, input_dev->keybit);
 	input_set_capability(input_dev, EV_KEY, KEY_UNDER_WATER);
 	set_bit(KEY_ON_WATER, input_dev->keybit);
