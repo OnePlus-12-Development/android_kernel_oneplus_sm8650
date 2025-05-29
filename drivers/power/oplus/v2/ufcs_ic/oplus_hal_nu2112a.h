ad3908b07b16e008b66e9bef1f0cb256eeba1a85
chandu078 <chandudyavanapelli03@gmail.com>
2025-05-29 12:15:24 +0800

    treewide: Update changes from OnePlus 12 CPH2581_15.0.0.820(EX01)
    Change-Id: Ibc9a1bad280b3b22e8dfe98f01672cf5526670e5
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- drivers/power/oplus/v2/ufcs_ic/oplus_hal_nu2112a.h
+++ drivers/power/oplus/v2/ufcs_ic/oplus_hal_nu2112a.h
@@ -238,6 +238,11 @@
 /* Register 3Ch */
 #define NU2112A_REG_3C 0x3C
 
+/* Register DEh */
+#define NU2112A_REG_DE 0xDE
+
+/* Register E7h */
+#define NU2112A_REG_E7 0xE7
 /************************UFCS REG***********************************/
 #define UFCS_BTB_TEMP_MAX 80
 #define UFCS_USB_TEMP_MAX 80
@@ -335,4 +340,5 @@
 #define NU2112A_MASK_SND_CMP BIT(2)
 
 #endif /*_OPLUS_NU2112A_H_*/
+
 
